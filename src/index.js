32489a75ac4fe5e593698613feb4ca4ab5310296
JAgie24 <misso_joseph@hotmail.ca>
2024-03-05 23:46:55 -0500

    added showConditions & showHumidity

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ function search(event) {
   let url = `https://api.shecodes.io/weather/v1/current?query=${city.value}&key=${apiKey}&units=metric`;
   axios.get(url).then(showTemp);
   axios.get(url).then(showWindSpeed);
+  axios.get(url).then(showConditions);
+  axios.get(url).then(showHumidity);
+  axios.get(url).then(showCity);
 }
 
 function formatDate(date) {
@@ -40,12 +43,28 @@ function showTemp(response) {
   let temperature = response.data.temperature.current;
   let temperatureValue = document.querySelector(".current-temperature-value");
   temperatureValue.innerHTML = Math.round(temperature);
-  console.log(temperature);
 }
 
 function showWindSpeed(response) {
-  let windSpeed = response.data;
-  console.log(windSpeed);
+  let windSpeed = response.data.wind.speed;
+  let wind = document.querySelector("#wind-speed");
+  wind.innerHTML = `${windSpeed}km/h`;
+}
+
+function showConditions(response) {
+  let conditions = response.data.condition.description;
+  let description = document.querySelector("#decription");
+  description.innerHTML = `, ${conditions}`;
+}
+
+function showHumidity(response) {
+  let humidity = response.data.temperature.humidity;
+  console.log(humidity);
+}
+
+function showCity(response) {
+  let city = response.data.city;
+  console.log(city);
 }
 
 let searchForm = document.querySelector("#search-form");
